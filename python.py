4478106aeac28c2d18a4bfe4a4910cc2fad00730
danilwww123 <dayeiil@internet.ru>
2023-10-12 15:27:21 +0500

    clown

--- python.py
+++ python.py
@@ -15,3 +15,19 @@ def create_note(notes, title, body):
     notes.append(new_note)
     save_notes(notes)
     print(f"Заметка '{title}' успешно создана.")
+# Функция для чтения списка заметок
+def list_notes(notes):
+    if notes:
+        print("Список заметок:")
+        for note in notes:
+            print(f"{note['id']}. {note['title']} (Дата: {note['timestamp']})")
+    else:
+        print("У вас нет заметок.")
+
+# Функция для чтения заметки по идентификатору
+def read_note(notes, note_id):
+    for note in notes:
+        if note["id"] == note_id:
+            print(f"Заметка '{note['title']}' (Дата: {note['timestamp']}):\n{note['body']}")
+            return
+    print(f"Заметка с идентификатором '{note_id}' не найдена.")
